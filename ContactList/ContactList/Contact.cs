899eaad8f0da7d4f2bfb5666833590474fdefe74
Jeffrey Nightengale <Jeffrey.Nightengale@ou.edu>
2021-09-22 15:11:42 -0500

    started

---
+++ ContactList/ContactList/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList
+{
+    class Contact
+    {
+        public int ID { get; set; }
+        public string First { get; set}
+        public string Last { get; set; }
+        public string Email { get; set; }
+
+        public string URL { get; set; }
+
+        public Contact()
+        {
+            ID = 0;
+            First = string.Empty;
+            Last = string.Empty;
+            Email = string.Empty;
+            URL = string.Empty;
+             
+        }
+
+
+    }
+}
